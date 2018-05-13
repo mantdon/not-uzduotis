998a3ff9c700b486ae30c15866eaa414778988c4
Mantvydas Donauskas <mantvydas.donauskas@ktu.edu>
2018-05-13 03:21:34 +0300

    Added Brewery Entity.

---
+++ src/Entity/Brewery.php
@@ -0,0 +1,272 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\BreweryRepository")
+ */
+class Brewery
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $address1;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $address2;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $state;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private $code;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $country;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $phone;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $website;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $lastModification;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Beer", mappedBy="brewery");
+     */
+    private $beers;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Geocode", mappedBy="brewery")
+     */
+    private $geocodes;
+
+    public function __construct()
+    {
+        $this->beers = new ArrayCollection();
+        $this->geocodes = new ArrayCollection();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAddress1(): ?string
+    {
+        return $this->address1;
+    }
+
+    public function setAddress1(?string $address1): self
+    {
+        $this->address1 = $address1;
+
+        return $this;
+    }
+
+    public function getAddress2(): ?string
+    {
+        return $this->address2;
+    }
+
+    public function setAddress2(?string $address2): self
+    {
+        $this->address2 = $address2;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(?string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getState(): string
+    {
+        return $this->state;
+    }
+
+    public function setState($state): self
+    {
+        $this->state = $state;
+        return $this;
+    }
+
+    public function getCode(): string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+        return $this;
+    }
+
+    public function getCountry(): string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
+        return $this;
+    }
+
+    public function getPhone(): string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): self
+    {
+        $this->phone = $phone;
+        return $this;
+    }
+
+    public function getWebsite(): string
+    {
+        return $this->website;
+    }
+
+    public function setWebsite(string $website): self
+    {
+        $this->website = $website;
+        return $this;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function getLastModification(): \DateTimeInterface
+    {
+        return $this->lastModification;
+    }
+
+    public function setLastModification(\DateTimeInterface $lastModification): self
+    {
+        $this->lastModification = $lastModification;
+        return $this;
+    }
+
+    public function getBeers(): ArrayCollection
+    {
+        return $this->beers;
+    }
+
+    public function addBeer(Beer $beer): self
+    {
+        if (!$this->beers->contains($beer)) {
+            $this->beers[] = $beer;
+            $beer->setBrewery($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBeer(Beer $beer): self
+    {
+        if ($this->beers->contains($beer)) {
+            $this->beers->removeElement($beer);
+            if ($beer->getBrewery() === $this) {
+                $beer->setBrewery(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function addGeocode(Geocode $geocode): self
+    {
+        if (!$this->geocodes->contains($geocode)) {
+            $this->geocodes[] = $geocode;
+            $geocode->setBrewery($this);
+        }
+
+        return $this;
+    }
+
+    public function removeGeocode(Geocode $geocode): self
+    {
+        if ($this->geocodes->contains($geocode)) {
+            $this->geocodes->removeElement($geocode);
+            if ($geocode->getBrewery() === $this) {
+                $geocode->setBrewery(null);
+            }
+        }
+
+        return $this;
+    }
+}
